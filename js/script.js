d7db5f668d868f3e214c950cc88abcd2d579cc87
AlexR <example@gmail.com>
2024-10-24 21:00:13 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// const result = confirm("are you human?");
+// console.log(result);
+ const answers = [];
+ answers[0] = prompt("what is your name?");
+ answers[1] = prompt("what is your last name?");
+ answers[2] = prompt("what is your age?");
+
+ document.write(answers);
