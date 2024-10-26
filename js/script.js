74159924c02f872534947ef1ef3e51655c094247
AlexR <example@gmail.com>
2024-10-26 20:01:00 +0300

    added cycles and conditions

--- js/script.js
+++ js/script.js
@@ -18,26 +18,32 @@ const personalMovieDB = {
     private: false
 };
 
-const movieName1 = prompt("please write a name of a movie you recently saw");
-const movieRate1 = prompt(`please rate "${movieName1}" from 1 to 10`);
-const movieName2 = prompt("please write a name of a movie you recently saw");
-const movieRate2 = prompt(`please rate "${movieName2}" from 1 to 10`);
 
-generateAnswer(movieName1, movieRate1);
-generateAnswer(movieName2, movieRate2);
-
-function generateAnswer(movieName, movieRate) {
+for (let i = 0; i < personalMovieDB.count; i++) {
     // debugger;
+    let movieName = prompt("please write a name of a movie you recently saw (1 - 10 symbols");
+    let movieRate = prompt(`please rate "${movieName}" from 1 to 50`);
+    if (movieName && movieName.length <= 50 && movieRate) {
+        generateAnswer(movieName, movieRate);
+        console.log("done");
+    } else {
+        console.log("error");
+        i--;
+    }
+}
+function generateAnswer(movieName, movieRate) {
     if (movieName && movieRate) {
         personalMovieDB.movies[movieName] = movieRate;
     }
 };
 console.log(personalMovieDB);
-
-
-
-// const rateMovie = function (movieName, movieRate) {
-//     movies.answer = movieRate;
-
-// };
+if (personalMovieDB.count < 2) {
+    console.log("you have to watch more movies");
+} else if (personalMovieDB.count >= 2 && personalMovieDB.count <= 3) {
+    console.log("you are good movieWatcher");
+} else if (personalMovieDB.count > 3) {
+    console.log("you are a fantastic!");
+}else{
+    console.log("something went wrong");
+}
 
