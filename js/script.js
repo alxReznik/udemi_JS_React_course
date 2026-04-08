1f32d662cc3e0b1f9400891840e9ca946076ef8d
AlexR <example@gmail.com>
2026-04-08 16:53:39 +0300

    added on click function to first heart

--- js/script.js
+++ js/script.js
@@ -6,35 +6,42 @@ const box = document.getElementById('box'),
     oneHeart = document.querySelector('.heart'),
     wrapper = document.querySelector('.wrapper');
 
-    // box.style.backgroundColor = 'blue';
-    // box.style.width = '500px';
-    
-    box.style.cssText = 'background-color:blue; width: 500px';
-    btns[1].style.borderRadius = '100%'
-    circles[0].style.backgroundColor = 'red';
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
 
-    // for(let i = 0; i < hearts.length; i++){
-    //     hearts[i].style.backgroundColor = 'green';
-    // }
-    hearts.forEach(item => {
-        item.style.backgroundColor = "yellow"
-    })
+box.style.cssText = 'background-color:blue; width: 500px';
+btns[1].style.borderRadius = '100%'
+circles[0].style.backgroundColor = 'red';
 
-    const div = document.createElement('div');
-    // const text = document.createTextNode('tut bil ya');
+// for(let i = 0; i < hearts.length; i++){
+//     hearts[i].style.backgroundColor = 'green';
+// }
+hearts.forEach(item => {
+    item.style.backgroundColor = "yellow"
+})
 
-    div.classList.add('black');
-    document.body.append(div)
-    // wrapper.appendChild(div)
-    // wrapper.insertBefore(div,hearts[2]);
-    // wrapper.append(div);
-    // wrapper.prepend(div);
-    // hearts[0].before(div)
-    // hearts[0].after(div)
-    // circles[1].remove();
-    // hearts[1].replaceWith(circles[0])
+const div = document.createElement('div');
+// const text = document.createTextNode('tut bil ya');
 
-    div.innerHTML = "<h1>Hello World</h1>";
-    // div.textContent = "Hello"
-    div.insertAdjacentHTML('', <h2>Hello</h2>)
+div.classList.add('black');
+document.body.append(div)
+// wrapper.appendChild(div)
+// wrapper.insertBefore(div,hearts[2]);
+// wrapper.append(div);
+// wrapper.prepend(div);
+// hearts[0].before(div)
+// hearts[0].after(div)
+// circles[1].remove();
+// hearts[1].replaceWith(circles[0])
 
+div.innerHTML = "<h1>Hello World</h1>";
+// div.textContent = "Hello"
+div.insertAdjacentHTML('', <h2>Hello</h2>)
+
+oneHeart.addEventListener('click', () => {
+    changeBackgroundColor(oneHeart);
+})
+
+function changeBackgroundColor(element) {
+    element.style.backgroundColor = "yellow";
+}
